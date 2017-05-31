6d2668c1cc891c0597a95c620835f4f61bf1c58c
Devan Moylan <dmmoylan@gmail.com>
2017-05-31 20:16:40 +0000

    Creation of Scraper class; creation of class method called scrape_scouts_newsfeed

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# recruiting-news-cli-app
-The latest news on high school athletes who aspire to play collegiate athletics. 
+The latest news on high school athletes who aspire to play collegiate athletics.
---
+++ lib/scraper.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'pry'
+
+class Scraper
+
+scouts_url = "http://www.scout.com/college/football/recruiting/news?type=stories&sortBy=Date&site=ScoutFootball.com"
+
+  def self.scrape_scouts_newsfeed(scouts_url)
+    doc = Nokogiri::HTML(open(scouts_url))
+    @posts = []
+    doc.css(".story-list-item").each_with_index do |post, index|
+      @posts[index] = {
+        :title => post.css(".story-deck h1 a span").last.text,
+        :author => post.css(".story-deck .story-from").text,
+        :time => post.css(".story-deck .time-stamp").text,
+        :description => post.css(".story-deck p").text,
+        :link => post.css(".story-deck .story-stuff a").attribute("href").value
+      }
+    end
+    @posts
+  end
+
+end
