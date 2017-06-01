22399ad7b9a1df0a00b5ab0b87b41b73ec790d02
Devan Moylan <dmmoylan@gmail.com>
2017-06-01 23:02:57 +0000

    wrapping it up

--- lib/recruitings_news/cli.rb
+++ lib/recruitings_news/cli.rb
@@ -13,21 +13,31 @@ class RecruitingsNews::CLI
 
   def news_source_options
     input = nil
-    while input != "exit"
+    while input != valid_exit
       puts "\n\nSelect a recruiting news source:"
       puts "    1 - for Scout \n    2 - for Bleacher Report \n    3 - for Rivals \n    exit - to exit"
       input = gets.strip.downcase
       case input
-      when "1" then puts "\nLoading News from Scout...\n"
+        when "1" then puts "\nLoading News from Scout...\n"
           load_news_from_scouts
-          puts "\n#{Post.all.count} posts from Scout loaded.\n"
         when "2" then puts "\nLoading News from Bleacher Report...\n"
           load_news_from_bleacher_report
         when "3" then puts "\nLoading News from Rivals...\n"
+          load_news_from_rivals
         when "exit" then puts "\nExiting news sources. Enjoy your day.\n"
+          break
         else puts "\nNot an option.\n"
       end
+      number_of_posts_loaded
     end
+  end
+
+  def number_of_posts_loaded
+    puts "\n#{Post.all.count} posts loaded.\n" unless Post.all.count == 0
+  end
+
+  def valid_exit
+    "exit" || "Exit"
   end
 
   def load_news_from_scouts
@@ -42,6 +52,12 @@ class RecruitingsNews::CLI
     print_out_news
   end
 
+  def load_news_from_rivals
+    scraper = Scraper.new(rivals_path)
+    scraper.run_scrape_on_rivals
+    print_out_news
+  end
+
   def print_out_news
     Post.all.each {|post| puts "\n\nSubject: #{post.title}\n#{post.author}\n#{post.description}\nLearn more at #{post.link}\n"}
   end
@@ -52,6 +68,10 @@ class RecruitingsNews::CLI
 
   def bleacher_report_path
     "http://bleacherreport.com/recruiting"
+  end
+
+  def rivals_path
+    "https://basketballrecruiting.rivals.com/more_news"
   end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,14 +21,27 @@ class Scraper
   end
 
   def gather_posts_from_bleacher_report
-    binding.pry
-    @page.css(".story-list-item").each_with_index do |post, index|
+    @page.css(".articleSummary").each_with_index do |post, index|
       @posts[index] = {
-        :title => post.css(".story-deck h1 a span").last.text,
-        :author => post.css(".story-deck .story-from").text,
-        :time => post.css(".story-deck .time-stamp").text,
-        :description => post.css(".story-deck p").text,
-        :link => post.css(".story-deck .story-stuff a").attribute("href").value
+        :title => post.css(".articleContent .articleTitle h3").text,
+        :author => post.css(".articleContent .authorInfo a .name").text + " " + post.css(".provider").text,
+        :time => "",
+        :description => "",
+        :link => post.css(".articleContent .articleTitle").attribute("href").value
+      }
+    return false if @posts.empty?
+    end
+  end
+
+  def gather_posts_from_rivals
+    @page.css(".medium-article").each_with_index do |post, index|
+      binding.pry
+      @posts[index] = {
+        :title => post.css("").text,
+        :author => post.css("").text + " " + post.css(".provider").text,
+        :time => "",
+        :description => "",
+        :link => post.css("").attribute("href").value
       }
     return false if @posts.empty?
     end
@@ -46,6 +59,12 @@ class Scraper
 
   def run_scrape_on_bleacher_report
     if !@page.nil? && gather_posts_from_bleacher_report
+      create_new_post
+    end
+  end
+
+  def run_scrape_on_rivals
+    if !@page.nil? && gather_posts_from_rivals
       create_new_post
     end
   end
