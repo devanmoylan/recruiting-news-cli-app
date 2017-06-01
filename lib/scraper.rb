212d0c1029085c3ad6748389f684dd960cdcf5d1
Devan Moylan <dmmoylan@gmail.com>
2017-06-01 01:47:34 +0000

    Scraper class #gather_posts_from_scouts method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,5 +13,17 @@ class Scraper
     @page = Nokogiri::HTML(open(path))
   end
 
-  
+  def gather_posts_from_scouts
+    @page.css(".story-list-item").each_with_index do |post, index|
+      @posts[index] = {
+        :title => post.css(".story-deck h1 a span").last.text,
+        :author => post.css(".story-deck .story-from").text,
+        :time => post.css(".story-deck .time-stamp").text,
+        :description => post.css(".story-deck p").text,
+        :link => post.css(".story-deck .story-stuff a").attribute("href").value
+      }
+    return false if @posts.empty?
+    end
+  end
+
 end
